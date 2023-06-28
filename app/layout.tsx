93b62f4e5fd97eccd827214c968a3014209ca215
莱奥斯 <621862169@qq.com>
2023-06-28 18:15:08 +0800

    add favicon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,22 +15,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <link
-        rel="icon"
-        href="https://tabooproject.org/assets/img/taboolib-icon.png"
-      />
-      <html lang="en">
-        <body className={inter.className}>
-          <TabooHeader />
-          <div className="h-full flex-grow">{children}</div>
-          <footer className="footer footer-center p-4 bg-base-300 text-base-content flex-shrink mt-8">
-            <div>
-              <p>Copyright © 2023 - All right reserved by TabooProject</p>
-            </div>
-          </footer>
-        </body>
-      </html>
-    </>
+    <html lang="en">
+      <body className={inter.className}>
+        <TabooHeader />
+        <div className="h-full flex-grow">{children}</div>
+        <footer className="footer footer-center p-4 bg-base-300 text-base-content flex-shrink mt-8">
+          <div>
+            <p>Copyright © 2023 - All right reserved by TabooProject</p>
+          </div>
+        </footer>
+      </body>
+    </html>
   );
 }
